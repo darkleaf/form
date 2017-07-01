deac4d5016a34b5a78a9da5015f1d6cbc9f42c55
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-07-02 00:20:43 +0400

    refactoring

--- src/darkleaf/form/demo.cljs
+++ src/darkleaf/form/demo.cljs
@@ -13,78 +13,58 @@
 
 (enable-console-print!)
 
-
 (s/def ::string string?)
 (s/def ::present-string (s/and string? #(not (string/blank? %))))
 #_(s/def ::email #(re-matches #"^\S+@\S+$" %))
 (s/def ::password #(< 8 (count %)))
 
-(defn add-nested-item [data]
-  (update data :root/nested
-          conj {:nested/id (random-uuid)
-                :nested/example-text ""}))
-
-(defn- build-ctx [data-atom spec]
-  (let [data @data-atom
-        errors (->> data (s/explain-data spec) (explain-data->errors))
-        update (fn [path f] (swap! data-atom update-in path f))]
-    (ctx/build data errors update)))
-
-
 (s/def :text/example-text ::present-string)
 (s/def :text/example-password (s/and ::present-string ::password))
 (s/def :text/data (s/keys :req [:text/example-text
                                 :text/example-password]))
-(defn text [data-atom]
-  (let [f (build-ctx data-atom :text/data)]
-    [:form
-     [bootstrap/text f :text/example-text]
-     [bootstrap/text f :text/example-password :type :password]]))
+(defn text [f]
+  [:form
+   [bootstrap/text f :text/example-text]
+   [bootstrap/text f :text/example-password :type :password]])
 
 (s/def :textarea/example ::present-string)
 (s/def :textarea/data (s/keys :req [:textarea/example]))
-(defn textarea [data-atom]
-  (let [f (build-ctx data-atom :textarea/data)]
-    [:form
-     [bootstrap/textarea f :textarea/example :rows 5]]))
+(defn textarea [f]
+  [:form
+   [bootstrap/textarea f :textarea/example :rows 5]])
 
 (s/def :select/example #{"foo" "bar"})
 (s/def :select/data (s/keys :req [:select/example]))
-(defn select [data-atom]
-  (let [f (build-ctx data-atom :select/data)]
+(defn select [f]
     [bootstrap/select f :select/example
-     :options [["foo" "Foo"] ["bar" "Bar"] ["buzz" "Buzz"]]]))
+     :options [["foo" "Foo"] ["bar" "Bar"] ["buzz" "Buzz"]]])
 
 (s/def :radio-select/example #{"foo" "bar"})
 (s/def :radio-select/data (s/keys :req [:radio-select/example]))
-(defn radio-select [data-atom]
-  (let [f (build-ctx data-atom :radio-select/data)]
-    [bootstrap/radio-select f :radio-select/example
-     :options [["foo" "Foo"] ["bar" "Bar"] ["buzz" "Buzz"]]]))
+(defn radio-select [f]
+  [bootstrap/radio-select f :radio-select/example
+   :options [["foo" "Foo"] ["bar" "Bar"] ["buzz" "Buzz"]]])
 
 (s/def :multi-select/example (s/with-gen
                                (s/and vector? #(every? #{"foo" "bar"} %) distinct?)
                                #(gen/vector-distinct (gen/elements #{"foo" "bar"}))))
 (s/def :multi-select/data (s/keys :req [:multi-select/example]))
-(defn multi-select [data-atom]
-  (let [f (build-ctx data-atom :multi-select/data)]
-    [bootstrap/multi-select f :multi-select/example
-     :options [["foo" "Foo"] ["bar" "Bar"] ["buzz" "Buzz"]]]))
+(defn multi-select [f]
+  [bootstrap/multi-select f :multi-select/example
+   :options [["foo" "Foo"] ["bar" "Bar"] ["buzz" "Buzz"]]])
 
 (s/def :checkbox/example true?)
 (s/def :checkbox/data (s/keys :req [:checkbox/example]))
-(defn checkbox [data-atom]
-  (let [f (build-ctx data-atom :checkbox/data)]
-    [bootstrap/checkbox f :checkbox/example]))
+(defn checkbox [f]
+  [bootstrap/checkbox f :checkbox/example])
 
 (s/def :nested/id uuid?)
 (s/def :nested/attribute ::present-string)
 (s/def :nested/item (s/keys :req [:nested/id :nested/attribute]))
 (s/def :nested/items (s/coll-of :nested/item :count 3))
 (s/def :nested/data (s/keys :req [:nested/items]))
-(defn nested [data-atom]
-  (let [f (build-ctx data-atom :nested/data)
-        items-f (ctx/nested f :nested/items)
+(defn nested [f]
+  (let [items-f (ctx/nested f :nested/items)
         build-blank-item (fn [] {:nested/id (random-uuid)
                                  :nested/attribute ""})]
 
@@ -102,9 +82,9 @@
      [bootstrap/add-nested items-f build-blank-item
       :class "btn-primary btn-sm"]]))
 
-(defn- inspect [data-atom spec]
-  (let [data @data-atom
-        errors (->> data (s/explain-data spec) (explain-data->errors))]
+(defn- inspect [f]
+  (let [data (ctx/get-data f)
+        errors (ctx/get-errors-subtree f)]
     [:div
      [:div.card.mb-3
       [:div.card-header "Data"]
@@ -115,24 +95,31 @@
       [:div.card-block
        [:pre [:code (with-out-str (pprint errors))]]]]]))
 
+(defn- build-ctx [data-atom spec]
+  (let [data @data-atom
+        errors (->> data (s/explain-data spec) (explain-data->errors))
+        update (fn [path f] (swap! data-atom update-in path f))]
+    (ctx/build data errors update)))
+
 (defn- container [title spec component]
   (let [data (-> spec s/gen gen/generate)
         data-atom (r/atom data)]
     (fn []
-      [:div
-       [:h2 title]
-       [component data-atom]
-       [inspect data-atom spec]])))
+      (let [f (build-ctx data-atom spec)]
+        [:div
+         [:h2 title]
+         [component f]
+         [inspect f]]))))
 
 (defn component []
   [:div
-    [container "Text" :text/data text]
-    [container "Textarea" :textarea/data textarea]
-    [container "Select" :select/data select]
-    [container "Radio select" :radio-select/data radio-select]
-    [container "Multi select" :multi-select/data multi-select]
-    [container "Checkbox" :checkbox/data checkbox]
-    [container "Nested" :nested/data nested]])
+   [container "Text" :text/data text]
+   [container "Textarea" :textarea/data textarea]
+   [container "Select" :select/data select]
+   [container "Radio select" :radio-select/data radio-select]
+   [container "Multi select" :multi-select/data multi-select]
+   [container "Checkbox" :checkbox/data checkbox]
+   [container "Nested" :nested/data nested]])
 
 (r/render [component]
           (.getElementById js/document "point"))
