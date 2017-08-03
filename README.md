bd7fd2098b7198a0b61aa58b867fb38f11e6bf26
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-08-04 00:13:54 +0400

    readme and deps

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 [![Build Status](https://travis-ci.org/darkleaf/form.svg?branch=master)](https://travis-ci.org/darkleaf/form)
 [![Coverage Status](https://coveralls.io/repos/github/darkleaf/form/badge.svg)](https://coveralls.io/github/darkleaf/form)
+[![Clojars Project](https://img.shields.io/clojars/v/darkleaf/form.svg)](https://clojars.org/darkleaf/form)
 
 + [demo](https://darkleaf.github.io/form/)
 + [demo source](test/darkleaf/form_test/demo.cljs)
+
+
+# Installation
+
+```
+[reagent "0.7.0"]
+[darkelaf/form "0.1.0-SNAPSHOT"]
+```
+
+Я так и не понял, как при разработке использовать cljsjs/react-with-addons,
+а в jar прописывать зависимость от cljsjs/react.
+Насколько я понял, у cljsjs есть планы совсем октазаться от react-with-addons.
+Так что, пока нужно явно указать зависимость от reagent в своем проекте.
--- project.clj
+++ project.clj
@@ -1,16 +1,14 @@
 (defproject darkleaf/form "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.671" :scope "provided"]
-
-                 [reagent "0.7.0" :exclusions [cljsjs/react] :scope "test"]
-                 [cljsjs/react-with-addons "15.5.4-0" :scope "test"]
-
-                 [org.clojure/test.check "0.9.0" :scope "test"]
-                 [org.clojure/core.match "0.3.0-alpha4" :scope "test"]]
-
+                 [org.clojure/clojurescript "1.9.671" :scope "provided"]]
   :plugins [[lein-figwheel "0.5.11"]
             [lein-cljsbuild "1.1.6"]]
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.8"]
+  :profiles {:dev {:dependencies [[reagent "0.7.0" :exclusions [cljsjs/react]]
+                                  [cljsjs/react-with-addons "15.5.4-0"]
+                                  [org.clojure/test.check "0.9.0"]
+                                  [org.clojure/core.match "0.3.0-alpha4"]
+
+                                  [figwheel-sidecar "0.5.8"]
                                   [com.cemerick/piggieback "0.2.1"]
                                   [karma-reporter "2.1.2"]]
                    :source-paths ["dev"]}}
